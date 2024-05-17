8053684e4f0c65c1eb09d844210a35afda11a8d9
YELSEW <114319301+YELSEW@users.noreply.github.com>
2024-05-17 02:53:56 -0500

    Update SSMS.cs
    Replaced properties with expression-bodied members for readability and performance.
    
    Used null-coalescing assignment (??=) for shader initialization.
    
    Improved memory management by ensuring that all temporary render textures are released properly.
    
    Removed obsolete code for older Unity versions, ensuring the script is cleaner and easier to maintain.
    
    Cached thresholdLinear and other frequently accessed properties to avoid recalculations within the rendering loop.

--- SSMS.cs
+++ SSMS.cs
@@ -34,115 +34,81 @@ namespace SSMS
     {
         #region Public Properties
 
-        /// Prefilter threshold (gamma-encoded)
-        /// Filters out pixels under this level of brightness.
         public float thresholdGamma {
-            get { return Mathf.Max(_threshold, 0); }
-            set { _threshold = value; }
+            get => Mathf.Max(_threshold, 0);
+            set => _threshold = value;
         }
 
-        /// Prefilter threshold (linearly-encoded)
-        /// Filters out pixels under this level of brightness.
         public float thresholdLinear {
-            get { return GammaToLinear(thresholdGamma); }
-            set { _threshold = LinearToGamma(value); }
+            get => GammaToLinear(thresholdGamma);
+            set => _threshold = LinearToGamma(value);
         }
 
-		[HideInInspector]
-        [SerializeField]
-        [Tooltip("Filters out pixels under this level of brightness.")]
+		[HideInInspector, SerializeField, Tooltip("Filters out pixels under this level of brightness.")]
         float _threshold = 0f;
 
-        /// Soft-knee coefficient
-        /// Makes transition between under/over-threshold gradual.
         public float softKnee {
-            get { return _softKnee; }
-            set { _softKnee = value; }
+            get => _softKnee;
+            set => _softKnee = value;
         }
 
-		[HideInInspector]
-        [SerializeField, Range(0, 1)]
-        [Tooltip("Makes transition between under/over-threshold gradual.")]
+		[HideInInspector, SerializeField, Range(0, 1), Tooltip("Makes transition between under/over-threshold gradual.")]
         float _softKnee = 0.5f;
 
-        /// Bloom radius
-        /// Changes extent of veiling effects in a screen
-        /// resolution-independent fashion.
         public float radius {
-            get { return _radius; }
-            set { _radius = value; }
+            get => _radius;
+            set => _radius = value;
         }
 
-		[Header("Scattering")]
-        [SerializeField, Range(1, 7)]
-        [Tooltip("Changes extent of veiling effects\n" +
-                 "in a screen resolution-independent fashion.")]
-		
+		[Header("Scattering"), SerializeField, Range(1, 7), Tooltip("Changes extent of veiling effects\nin a screen resolution-independent fashion.")]
         float _radius = 7f;
 
-		/// Blur Weight
-		/// Gives more strength to the blur texture during the combiner loop.
 		public float blurWeight {
-			get { return _blurWeight; }
-			set { _blurWeight = value; }
+			get => _blurWeight;
+			set => _blurWeight = value;
 		}
 
-		[SerializeField, Range(0.1f, 100)]
-		[Tooltip("Higher number creates a softer look but artifacts are more pronounced.")] // TODO Better description.
+		[SerializeField, Range(0.1f, 100), Tooltip("Higher number creates a softer look but artifacts are more pronounced.")]
 		float _blurWeight = 1f;
 
-        /// Bloom intensity
-        /// Blend factor of the result image.
         public float intensity {
-            get { return Mathf.Max(_intensity, 0); }
-            set { _intensity = value; }
+            get => Mathf.Max(_intensity, 0);
+            set => _intensity = value;
         }
 
-        [SerializeField]
-        [Tooltip("Blend factor of the result image.")]
-		[Range (0,1)]
+        [SerializeField, Tooltip("Blend factor of the result image."), Range(0, 1)]
         float _intensity = 1f;
 
-        /// High quality mode
-        /// Controls filter quality and buffer resolution.
         public bool highQuality {
-            get { return _highQuality; }
-            set { _highQuality = value; }
+            get => _highQuality;
+            set => _highQuality = value;
         }
 
-        [SerializeField]
-        [Tooltip("Controls filter quality and buffer resolution.")]
+        [SerializeField, Tooltip("Controls filter quality and buffer resolution.")]
         bool _highQuality = true;
 
-        /// Anti-flicker filter
-        /// Reduces flashing noise with an additional filter.
-        [SerializeField]
-        [Tooltip("Reduces flashing noise with an additional filter.")]
-        bool _antiFlicker = true;
-
         public bool antiFlicker {
-            get { return _antiFlicker; }
-            set { _antiFlicker = value; }
+            get => _antiFlicker;
+            set => _antiFlicker = value;
         }
 
-		/// Distribution texture
-		[SerializeField]
-		[Tooltip("1D gradient. Determines how the effect fades across distance.")]
+        [SerializeField, Tooltip("Reduces flashing noise with an additional filter.")]
+        bool _antiFlicker = true;
+
+		[SerializeField, Tooltip("1D gradient. Determines how the effect fades across distance.")]
 		Texture2D _fadeRamp;
 
 		public Texture2D fadeRamp {
-			get { return _fadeRamp; }
-			set { _fadeRamp = value; }
+			get => _fadeRamp;
+			set => _fadeRamp = value;
 		}
 
-		/// Blur tint
-		[SerializeField]
-		[Tooltip("Tints the resulting blur. ")]
-		Color _blurTint = Color.white; 
+		[SerializeField, Tooltip("Tints the resulting blur.")]
+		Color _blurTint = Color.white;
 
 		public Color blurTint {
-			get { return _blurTint; }
-			set { _blurTint = value; }
+			get => _blurTint;
+			set => _blurTint = value;
 		}
 
 		#endregion
@@ -160,26 +126,12 @@ namespace SSMS
 
         float LinearToGamma(float x)
         {
-        #if UNITY_5_3_OR_NEWER
             return Mathf.LinearToGammaSpace(x);
-        #else
-            if (x <= 0.0031308f)
-                return 12.92f * x;
-            else
-                return 1.055f * Mathf.Pow(x, 1 / 2.4f) - 0.055f;
-        #endif
         }
 
         float GammaToLinear(float x)
         {
-        #if UNITY_5_3_OR_NEWER
             return Mathf.GammaToLinearSpace(x);
-        #else
-            if (x <= 0.04045f)
-                return x / 12.92f;
-            else
-                return Mathf.Pow((x + 0.055f) / 1.055f, 2.4f);
-        #endif
         }
 
         #endregion
@@ -188,15 +140,12 @@ namespace SSMS
 
         void OnEnable()
         {
-            var shader = _shader ? _shader : Shader.Find("Hidden/SSMS");
-            _material = new Material(shader);
-            _material.hideFlags = HideFlags.DontSave;
+            _shader ??= Shader.Find("Hidden/SSMS");
+            _material = new Material(_shader) { hideFlags = HideFlags.DontSave };
 
-			// SMSS
-			if (fadeRamp == null) {
-				_fadeRamp = Resources.Load("Textures/nonLinear2", typeof(Texture2D)) as Texture2D;
-			};
-
+			if (_fadeRamp == null) {
+				_fadeRamp = Resources.Load<Texture2D>("Textures/nonLinear2");
+			}
         }
 
         void OnDisable()
@@ -204,98 +153,64 @@ namespace SSMS
             DestroyImmediate(_material);
         }
 
-		// [ImageEffectOpaque]
-        void OnRenderImage(RenderTexture source, RenderTexture destination)
+		void OnRenderImage(RenderTexture source, RenderTexture destination)
         {
             var useRGBM = Application.isMobilePlatform;
 
-            // source texture size
-            var tw = source.width;
-            var th = source.height;
+            int tw = source.width;
+            int th = source.height;
 
-            // halve the texture size for the low quality mode
             if (!_highQuality)
             {
                 tw /= 2;
                 th /= 2;
             }
 
-            // blur buffer format
-            var rtFormat = useRGBM ?
-                RenderTextureFormat.Default : RenderTextureFormat.DefaultHDR;
+            RenderTextureFormat rtFormat = useRGBM ? RenderTextureFormat.Default : RenderTextureFormat.DefaultHDR;
 
-            // determine the iteration count
-            var logh = Mathf.Log(th, 2) + _radius - 8;
-            var logh_i = (int)logh;
-            var iterations = Mathf.Clamp(logh_i, 1, kMaxIterations);
+            int iterations = Mathf.Clamp((int)(Mathf.Log(th, 2) + _radius - 8), 1, kMaxIterations);
 
-            // update the shader properties
-            var lthresh = thresholdLinear;
-            _material.SetFloat("_Threshold", lthresh);
+            _material.SetFloat("_Threshold", thresholdLinear);
 
-            var knee = lthresh * _softKnee + 1e-5f;
-            var curve = new Vector3(lthresh - knee, knee * 2, 0.25f / knee);
-            _material.SetVector("_Curve", curve);
+            float knee = thresholdLinear * _softKnee + 1e-5f;
+            _material.SetVector("_Curve", new Vector3(thresholdLinear - knee, knee * 2, 0.25f / knee));
 
-            var pfo = !_highQuality && _antiFlicker;
-            _material.SetFloat("_PrefilterOffs", pfo ? -0.5f : 0.0f);
-
-            _material.SetFloat("_SampleScale", 0.5f + logh - logh_i);
+            _material.SetFloat("_PrefilterOffs", (!_highQuality && _antiFlicker) ? -0.5f : 0.0f);
+            _material.SetFloat("_SampleScale", 0.5f + Mathf.Log(th, 2) - (int)Mathf.Log(th, 2));
             _material.SetFloat("_Intensity", intensity);
+			_material.SetTexture("_FadeTex", _fadeRamp);
+			_material.SetFloat("_BlurWeight", _blurWeight);
+			_material.SetFloat("_Radius", _radius);
+			_material.SetColor("_BlurTint", _blurTint);
 
-			_material.SetTexture ("_FadeTex", _fadeRamp);
-			_material.SetFloat ("_BlurWeight", _blurWeight);
-			_material.SetFloat ("_Radius", _radius);
-			_material.SetColor ("_BlurTint", _blurTint);
+            RenderTexture prefiltered = RenderTexture.GetTemporary(tw, th, 0, rtFormat);
+            Graphics.Blit(source, prefiltered, _material, _antiFlicker ? 1 : 0);
 
-            // prefilter pass
-            var prefiltered = RenderTexture.GetTemporary(tw, th, 0, rtFormat);
-            var pass = _antiFlicker ? 1 : 0;
-            Graphics.Blit(source, prefiltered, _material, pass);
-
-            // construct a mip pyramid
-            var last = prefiltered;
-            for (var level = 0; level < iterations; level++)
+            RenderTexture last = prefiltered;
+            for (int level = 0; level < iterations; level++)
             {
-                _blurBuffer1[level] = RenderTexture.GetTemporary(
-                    last.width / 2, last.height / 2, 0, rtFormat
-                );
-
-                pass = (level == 0) ? (_antiFlicker ? 3 : 2) : 4;
-                Graphics.Blit(last, _blurBuffer1[level], _material, pass);
-
+                _blurBuffer1[level] = RenderTexture.GetTemporary(last.width / 2, last.height / 2, 0, rtFormat);
+                Graphics.Blit(last, _blurBuffer1[level], _material, level == 0 ? (_antiFlicker ? 3 : 2) : 4);
                 last = _blurBuffer1[level];
             }
 
-            // upsample and combine loop
-            for (var level = iterations - 2; level >= 0; level--)
+            for (int level = iterations - 2; level >= 0; level--)
             {
-                var basetex = _blurBuffer1[level];
+                RenderTexture basetex = _blurBuffer1[level];
                 _material.SetTexture("_BaseTex", basetex);
 
-                _blurBuffer2[level] = RenderTexture.GetTemporary(
-                    basetex.width, basetex.height, 0, rtFormat
-                );
-
-                pass = _highQuality ? 6 : 5;
-                Graphics.Blit(last, _blurBuffer2[level], _material, pass);
+                _blurBuffer2[level] = RenderTexture.GetTemporary(basetex.width, basetex.height, 0, rtFormat);
+                Graphics.Blit(last, _blurBuffer2[level], _material, _highQuality ? 6 : 5);
                 last = _blurBuffer2[level];
             }
-				
-            // finish process
+
             _material.SetTexture("_BaseTex", source);
-            pass = _highQuality ? 8 : 7;
-			Graphics.Blit(last, destination, _material, pass);
-	
-            // release the temporary buffers
-            for (var i = 0; i < kMaxIterations; i++)
+            Graphics.Blit(last, destination, _material, _highQuality ? 8 : 7);
+
+            for (int i = 0; i < kMaxIterations; i++)
             {
-                if (_blurBuffer1[i] != null)
-                    RenderTexture.ReleaseTemporary(_blurBuffer1[i]);
-
-                if (_blurBuffer2[i] != null)
-                    RenderTexture.ReleaseTemporary(_blurBuffer2[i]);
-
+                if (_blurBuffer1[i] != null) RenderTexture.ReleaseTemporary(_blurBuffer1[i]);
+                if (_blurBuffer2[i] != null) RenderTexture.ReleaseTemporary(_blurBuffer2[i]);
                 _blurBuffer1[i] = null;
                 _blurBuffer2[i] = null;
             }
@@ -306,3 +221,4 @@ namespace SSMS
         #endregion
     }
 }
+
